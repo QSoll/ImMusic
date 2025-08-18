17cc18707b13c281c1086d19bd974366fffd97ad
Sol Morcillo <sol.morcillo@yahoo.com>
2025-08-18 15:17:26 -0300

    at

--- src/main/java/com/immusic/model/Artista.java
+++ src/main/java/com/immusic/model/Artista.java
@@ -1,15 +1,13 @@
 package com.immusic.model;
 
-//import com.immusic.model.MusicaLink;
-
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
+@Data
 @Document(collection = "artistas")
 public class Artista {
-
-    
 
     @Id
     private String id;
@@ -56,138 +54,4 @@ public class Artista {
     // Biografia e imagem
     private String biografia;
     private String fotoUrl;
-
-    // Getters usados no controller ou frontend
-    public String getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getWhatsapp() {
-        return whatsapp;
-    }
-
-    public String getCidadeUf() {
-        return cidadeUf;
-    }
-
-    public String getBanda() {
-        return banda;
-    }
-
-    public String getEspecialidade() {
-        return especialidade;
-    }
-
-    public List<String> getEstilosMusicais() {
-        return estilosMusicais;
-    }
-
-    public List<String> getInstrumentos() {
-        return instrumentos;
-    }
-
-    public String getFacebook() {
-        return facebook;
-    }
-
-    public String getGithub() {
-        return github;
-    }
-
-    public String getInstagram() {
-        return instagram;
-    }
-
-    public String getKwai() {
-        return kwai;
-    }
-
-    public String getLinkedin() {
-        return linkedin;
-    }
-
-    public String getThreads() {
-        return threads;
-    }
-
-    public String getTiktok() {
-        return tiktok;
-    }
-
-    public String getTwitter() {
-        return twitter;
-    }
-
-    public String getYoutube() {
-        return youtube;
-    }
-
-    public String getAmazonMusic() {
-        return amazonMusic;
-    }
-
-    public String getAppleMusic() {
-        return appleMusic;
-    }
-
-    public String getBandcamp() {
-        return bandcamp;
-    }
-
-    public String getDeezer() {
-        return deezer;
-    }
-
-    public String getPalcom3() {
-        return palcom3;
-    }
-
-    public String getResso() {
-        return resso;
-    }
-
-    public String getSoundcloud() {
-        return soundcloud;
-    }
-
-    public String getSpotify() {
-        return spotify;
-    }
-
-    public String getSuaMusica() {
-        return suaMusica;
-    }
-
-    public String getTidal() {
-        return tidal;
-    }
-
-    public String getYoutubeMusic() {
-        return youtubeMusic;
-    }
-
-    public String getBiografia() {
-        return biografia;
-    }
-
-    public String getFotoUrl() {
-        return fotoUrl;
-    }
-
-    public List<MusicaLink> getMusicasIndividuais() {
-        return musicasIndividuais;
-    }
-
-    public void setMusicasIndividuais(List<MusicaLink> musicasIndividuais) {
-        this.musicasIndividuais = musicasIndividuais;
-    }
-
 }
